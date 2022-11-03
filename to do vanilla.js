5f0d168dfebe9dc692faa70f119a791750863d60
HosseinKM2000 <111495578+HosseinKM2000@users.noreply.github.com>
2022-11-03 13:59:52 +0330

    create addition method

--- to do vanilla.js
+++ to do vanilla.js
@@ -1,32 +1,36 @@
+const KEY_ENTER = 13;
+
 class todo{
     constructor(todoInputId , todoListId){
       this.todoInput = document.getElementById(todoInputId);
-      this.todoList = document.getElementById(todoListId);
-      this.todos = ['read the book','make some coffee'];
+      this.todosBox = document.getElementById(todoListId);
+      this.todos = [];
+
+      this.todoInput.addEventListener('keyup', (e) => {
+        if(e.keyCode == KEY_ENTER){
+          this.addtodo();
+        }
+      })
 
       this.render();
     }
-    // <li class="mb-2 d-flex justify-content-between border-1 border-light border p-1 ">
-    // read the book
-    // <span class="actions">
-    //     <a class="delete d-flex justify-content-center align-items-center btn btn-secondary">delete</a>
-    // </span>
-    // </li>
+ 
 
     render(){
        const todosOutput = document.createElement('ul');
-       const input = document.querySelector('#todo-input');
 
        todosOutput.classList.add('mt-5', 'text-white' , 'w-100');
        todosOutput.id = 'todo-list';
        
        for(const todo of this.todos){
          todosOutput.append(this.renderTodos(todo));
-         input.after(todosOutput)
        }
 
-       this.todoList.innerText = '';
+       this.todosBox.innerText = '';
+       this.todosBox.append(todosOutput);
     }
+
+
 
     renderTodos(todo){
       const li = document.createElement('li');
@@ -45,7 +49,16 @@ class todo{
 
       return li ;
     }
+
+
+
+    
+    addtodo(){
+      const todo = this.todoInput.value;
+      this.todos.push(todo);
+      this.render();
+    }
 }
 
-new todo('todo-input','todo-list');
+new todo('todo-input','todos-box');
 
